143ffcea8de627ac3fcdcd4976ff2dc7020dbb7b
Sasha Chun <your_email@example.com>
2025-06-01 17:43:12 +0300

    new auth app

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQnOtTjmK7DmzJxe3AwEeNBZl0gYK5IQw",
-  authDomain: "kotiki-auth.firebaseapp.com",
-  projectId: "kotiki-auth",
-  storageBucket: "kotiki-auth.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:1234567890abcdef123456"
+  apiKey: "AIzaSyDyiFSMCIs1NF5yOPFRJm6z41de8xk77gc",
+  authDomain: "kitty-40c6f.firebaseapp.com",
+  projectId: "kitty-40c6f",
+  storageBucket: "kitty-40c6f.firebasestorage.app",
+  messagingSenderId: "465460685030",
+  appId: "1:465460685030:web:06353fb1b1e4b90bee7327",
+  measurementId: "G-F89SHKB80D"
 };
 
 // Initialize Firebase
